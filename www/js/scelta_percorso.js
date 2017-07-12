fe6e6087ea31e1021e72050128d64a0f8e3affc4
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-07-12 18:52:19 +0200

    Created dynamic button to choose the route type

---
+++ www/js/scelta_percorso.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    typeRoute();
+});
+
+function typeRoute() {
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var allRoute = JSON.parse(xmlhttp.responseText);
+            parseRoute(allRoute);
+        }
+    };
+    xmlhttp.open("GET", "http://progettoindoor.altervista.org/getAllTypeRoute.php", true);
+    xmlhttp.send();
+}
+
+function parseRoute(allRoute) {
+    var out = "";
+    var i;
+    for (i = 0; i < allRoute.length; i++) {
+        out += '<button id=\"' + allRoute[i].Tipo + 
+                    '" class="button" type="button" onclick="window.location.href=\'percorso.html\';">' + 
+                    allRoute[i].Descrizione + '</button>';
+    }
+    document.getElementById("route-choice").innerHTML = out;
+}
